8b8e053ca7244bea56d4ee0a9e77b57c3230025c
Akash Bhatthal <akashbhatthal@gmail.com>
2024-10-23 14:57:44 -0700

    Start download + validation

--- src/main/java/org/scec/getfile/GetFile.java
+++ src/main/java/org/scec/getfile/GetFile.java
@@ -6,13 +6,23 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 //import com.google.gson.stream.JsonReader;
 //import com.google.gson.stream.JsonToken;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 //import java.io.FileReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 
@@ -23,10 +33,9 @@ import java.io.InputStreamReader;
  */
 public class GetFile {
 	public GetFile(String jsonFile) {
-		JsonObject json = parseJSON(jsonFile);	
+		JsonObject json = parseJson(jsonFile);	
 		server_ = json.get("server").toString().replaceAll("\"", "");
-		metadata_file_ = server_.concat(
-				json.get("app_meta").toString()).replaceAll("\"", "");
+		// TODO: Check if server is up. If not, log failure to connect and return
 
 		// For each file
 		//  * if file["version"] != latestVersion
@@ -35,52 +44,107 @@ public class GetFile {
 		//	  * downloadFile using validateFile with n tries
 	}
 	
-	private JsonObject parseJSON(String jsonFile) {
+	/**
+	 * Read a JSON file into memory for evaluation
+	 * @param jsonFile		Path to a JSON file
+	 * @return
+	 */
+	private JsonObject parseJson(String jsonFile) {
 		// https://stackoverflow.com/a/62106829
 		JsonObject json = null;
 		try (Reader reader = new InputStreamReader(
 				new FileInputStream(jsonFile), "UTF-8")) {
 		    json = (JsonObject) JsonParser.parseReader(reader);
 		} catch (FileNotFoundException e) {
-			System.err.println("GetFile.parseJSON FileNotFound");
+			System.err.println("GetFile.parseJson FileNotFound");
 			throw new RuntimeException(e);
 		} catch (UnsupportedEncodingException e) {
-			System.err.println("GetFile.parseJSON UnsupportedEncoding");
+			System.err.println("GetFile.parseJson UnsupportedEncoding");
 			throw new RuntimeException(e);
 		} catch (IOException e) {
-			System.err.println("GetFile.parseJSON IOException");
+			System.err.println("GetFile.parseJson IOException");
 			throw new RuntimeException(e);
 		}
 		return json;
 	}
 	
-	private String latestVersion() {
-		// Get the latestVersion of this file on the server
-		return "";
+	/**
+	 * Finds the latest version of a file given the server cached metadata
+	 * @param file		Key in the meta.json file. Not necessarily filename.
+	 * @return 			version of the given file
+	 */
+	private String latestVersion(String file) {
+		return ""; // TODO
 	}
 	
-	private void downloadFile() {
+	/**
+	 * Downloads a file with MD5 validation
+	 * @param fileUrl				URL of file to download
+	 * @param saveLocation			Where the downloaded file should be stored
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 * @return 0 if success and 1 if any failure
+	 */
+	private int downloadFile(String fileUrl, String saveLocation) throws IOException, URISyntaxException {
+		// Download the file from the given URL
+        File file = new File(".".concat(saveLocation));
+        FileUtils.copyURLToFile(new URI(fileUrl).toURL(), file);
+		 // Calculate the MD5 checksum of the downloaded file
+        String calculatedMd5 =
+        		DigestUtils.md5Hex(Files.newInputStream(file.toPath()));
+		if (calculatedMd5.equalsIgnoreCase(getExpectedMd5(fileUrl))) {
+			// Move hidden file to overwrite.
+			FileUtils.copyFile(file, new File(saveLocation));
+			file.delete();
+			System.out.printf("GetFile.downloadFile updated %s from %s", saveLocation, fileUrl);
+			return 0;
+		}
+		System.err.printf("GetFile.downloadFile Failed to download %s", fileUrl);
+		return 1;
+	}
+	
+	private String getExpectedMd5(String fileUrl)
+			throws URISyntaxException, MalformedURLException, IOException {
+		URI uri = new URI(fileUrl.concat(".md5"));
+		InputStream inputStream = uri.toURL().openStream();
+		return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
 		
 	}
 	
+	/**
+	 * Prompt user with JOptionPane if they want to update to latest version of file
+	 */
 	private void promptDownload() {
-		
+		// In the event of a manual update type, prompt the user prior to download
+		// TODO
 	}
 	
-	private void validateFile() {
-		
-	}
-
+	/**
+	 * Gets URL to the server
+	 * @return private server variable
+	 */
 	public String getServer() {
 		// Server to get latest files and metadata
 		return server_;
 	}
 	private String server_;
 	
-	public String getMeta() {
-		// Get full path to the metadata file on the server
-		return metadata_file_;
+	/**
+	 * Reads the meta.json file outlining versioned files to check for updates
+	 * @return JSON of all files on server and their latest versions
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public JsonObject getMeta() throws IOException, URISyntaxException {
+		// Try up to `retries` times to download
+		int status = 1;
+		final int retries = 3;
+		for (int i = 0; i < retries && status != 0; i++) {
+			status = downloadFile(server_.concat("meta.json"), "meta.json");
+		}
+		if (status != 0) {
+			return (JsonObject)null;
+		}
+		return parseJson("meta.json");
 	}
-	private String metadata_file_;
-	
 }
--- src/test/java/org/scec/getfile/GetFileTest.java
+++ src/test/java/org/scec/getfile/GetFileTest.java
@@ -1,17 +1,60 @@
 package org.scec.getfile;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-//import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+//import java.io.IOException;
+//import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+/**
+ * Test GetFile for correct JSON parsing and server response
+ */
 public class GetFileTest {
-	static GetFile getfile;
+
+	private static GetFile getfile;
+	private static WireMockServer wireMockServer;
 
 	@BeforeAll
 	public static void setUp() {
+		// Set up GetFile instance
 		getfile = new GetFile("src/test/resources/getfile.json");
+
+		// Initialize WireMock server on port 8080
+		// This server is our host for updated files and file metadata
+        wireMockServer =
+        		new WireMockServer(WireMockConfiguration.wireMockConfig().port(8080));
+        wireMockServer.start();
+        
+        // Configure WireMock to mock endpoint that serves a JSON file
+        wireMockServer.stubFor(get("/meta.json")
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("src/test/resources/meta.json")));
+        wireMockServer.stubFor(get("/meta.json.md5")
+                .willReturn(aResponse()
+                        .withBodyFile("src/test/resources/meta.json.md5")));
 	}
+	
+	@AfterAll
+    public static void tearDown() {
+        wireMockServer.stop();
+    }
 
 	// https://docs.gradle.org/current/samples/sample_building_java_libraries.html
 	@Test
@@ -19,13 +62,10 @@ public class GetFileTest {
 		assertEquals(getfile.getServer(),
 				"http://localhost:8080/");
 	}
-
+	
 	@Test
-	public void getMeta() {
-		assertEquals(getfile.getMeta(),
-				"http://localhost:8080/demo/meta.json");
+	public void getMeta() throws IOException, URISyntaxException {
+		assertNotNull(getfile.getMeta());
 	}
-
-	// TODO: Use WireMock to mock file server at localhost:8080
-	// https://stackoverflow.com/questions/606352/how-to-mock-a-web-server-for-unit-testing-in-java
+	
 }
