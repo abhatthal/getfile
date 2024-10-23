a61bdc8a58b69ea0c856bd468dd7f8c063107fbd
Akash Bhatthal <akashbhatthal@gmail.com>
2024-10-22 18:02:14 -0700

    Parse server and path to metadata file from json

--- src/main/java/org/scec/getfile/GetFile.java
+++ src/main/java/org/scec/getfile/GetFile.java
@@ -1,36 +1,59 @@
 package org.scec.getfile;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
+//import com.google.gson.JsonArray;
+//import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
+//import com.google.gson.stream.JsonReader;
+//import com.google.gson.stream.JsonToken;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+//import java.io.FileReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
-/* A GetFile instance contains all the logic required
+/**
+ * A GetFile instance contains all the logic required
  * to download and validate a file if a new version exists.
  * All tracked files must be versioned.
  */
 public class GetFile {
-	public GetFile(String JsonString) {
-		// Read a JSON of files to check for downloads
-		try {
-			JsonReader reader = new JsonReader(new FileReader(JsonString));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// Read "server"
+	public GetFile(String jsonFile) {
+		JsonObject json = parseJSON(jsonFile);	
+		server_ = json.get("server").toString().replaceAll("\"", "");
+		metadata_file_ = server_.concat(
+				json.get("app_meta").toString()).replaceAll("\"", "");
+
 		// For each file
 		//  * if file["version"] != latestVersion
 		//    * if not file["uploadType"]==automatic
 		//      then promptDownload
 		//	  * downloadFile using validateFile with n tries
-		
 	}
-
+	
+	private JsonObject parseJSON(String jsonFile) {
+		// https://stackoverflow.com/a/62106829
+		JsonObject json = null;
+		try (Reader reader = new InputStreamReader(
+				new FileInputStream(jsonFile), "UTF-8")) {
+		    json = (JsonObject) JsonParser.parseReader(reader);
+		} catch (FileNotFoundException e) {
+			System.err.println("GetFile.parseJSON FileNotFound");
+			throw new RuntimeException(e);
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("GetFile.parseJSON UnsupportedEncoding");
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			System.err.println("GetFile.parseJSON IOException");
+			throw new RuntimeException(e);
+		}
+		return json;
+	}
+	
 	private String latestVersion() {
 		// Get the latestVersion of this file on the server
 		return "";
@@ -47,4 +70,17 @@ public class GetFile {
 	private void validateFile() {
 		
 	}
+
+	public String getServer() {
+		// Server to get latest files and metadata
+		return server_;
+	}
+	private String server_;
+	
+	public String getMeta() {
+		// Get full path to the metadata file on the server
+		return metadata_file_;
+	}
+	private String metadata_file_;
+	
 }
--- src/test/java/org/scec/getfile/GetFileTest.java
+++ src/test/java/org/scec/getfile/GetFileTest.java
@@ -1,10 +1,30 @@
 package org.scec.getfile;
 
-import org.scec.getfile.GetFile;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+//import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class GetFileTest {
-	// TODO: Unit tests
+	static GetFile getfile;
+
+	@BeforeAll
+	public static void setUp() {
+		getfile = new GetFile("src/test/resources/getfile.json");
+	}
+
 	// https://docs.gradle.org/current/samples/sample_building_java_libraries.html
+	@Test
+	public void getServer() {
+		assertEquals(getfile.getServer(),
+				"http://localhost:8080/");
+	}
+
+	@Test
+	public void getMeta() {
+		assertEquals(getfile.getMeta(),
+				"http://localhost:8080/demo/meta.json");
+	}
 
 	// TODO: Use WireMock to mock file server at localhost:8080
 	// https://stackoverflow.com/questions/606352/how-to-mock-a-web-server-for-unit-testing-in-java
